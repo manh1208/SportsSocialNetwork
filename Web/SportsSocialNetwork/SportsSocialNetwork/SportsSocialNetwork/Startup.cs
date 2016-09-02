bc00e5358967465691c62a3b0535267ad990e30f
Manh Nguyen Van <manhnv1208@gmail.com>
2016-09-02 15:34:56 +0700

    [ManhNV] - Init Project Web

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportsSocialNetwork.Startup))]
+namespace SportsSocialNetwork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
