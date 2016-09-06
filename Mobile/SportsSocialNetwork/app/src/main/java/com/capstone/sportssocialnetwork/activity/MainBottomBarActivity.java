23c41e10b0870162ebbb470ea8a318a51ebec6b8
Manh Nguyen Van <manhnv1208@gmail.com>
2016-09-06 14:06:49 +0700

    [ManhNV] - Design main layout

---
+++ Mobile/SportsSocialNetwork/app/src/main/java/com/capstone/sportssocialnetwork/activity/MainBottomBarActivity.java
@@ -0,0 +1,81 @@
+package com.capstone.sportssocialnetwork.activity;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.capstone.sportssocialnetwork.R;
+import com.capstone.sportssocialnetwork.fragment.BookingFragment;
+import com.capstone.sportssocialnetwork.fragment.FeedFragment;
+import com.capstone.sportssocialnetwork.fragment.ProfileFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabSelectedListener;
+
+public class MainBottomBarActivity extends AppCompatActivity {
+    private FrameLayout frame_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bottom_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Trang chủ");
+        initView();
+        prepareView();
+        BottomBar bottomBar = BottomBar.attach(this,savedInstanceState);
+        bottomBar.setItemsFromMenu(R.menu.bottom_bar_menu, new OnMenuTabSelectedListener() {
+            @Override
+            public void onMenuItemSelected(@IdRes int menuItemId) {
+                Fragment fragment = null;
+                switch (menuItemId){
+                    case R.id.nav_feed:
+                        fragment = new FeedFragment();
+                        getSupportActionBar().setTitle("Trang chủ");
+                        break;
+                    case R.id.nav_booking:
+                        fragment = new BookingFragment();
+                        getSupportActionBar().setTitle("Đặt sân");
+                        break;
+                    case R.id.nav_profile:
+                        fragment = new ProfileFragment();
+                        getSupportActionBar().setTitle("Cá nhân");
+                        break;
+                }
+
+                if (fragment!=null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frame_menu, fragment)
+                            .commit();
+                }else{
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.frame_menu, new FeedFragment())
+                            .commit();
+                }
+            }
+        });
+    }
+
+    private void prepareView() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_menu, new FeedFragment())
+                .commit();
+    }
+
+    private void initView() {
+        frame_main = (FrameLayout) findViewById(R.id.frame_menu);
+    }
+
+
+}
