a7d51b7b7c79a1be4d0a74a8faa87d5fbd6aaba1
dev-huytv <huy.tran@wisky.vn>
2016-09-21 14:32:22 +0700

    [huytv] - user search place

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Controllers/PlaceController.cs
@@ -0,0 +1,74 @@
+﻿using SportsSocialNetwork.Models;
+using SportsSocialNetwork.Models.Entities;
+using SportsSocialNetwork.Models.Entities.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsSocialNetwork.Controllers
+{
+    public class PlaceController : Controller
+    {
+        // GET: Place
+        private readonly IPlaceService _placeService;
+
+        public PlaceController(IPlaceService placeService)
+        {
+            _placeService = placeService;
+        }
+        public ActionResult Index()
+        {
+            //var placeList = _placeService.getAllPlace();
+            //ViewBag.list = placeList;
+            return View();
+        }
+
+        public ActionResult GetData(JQueryDataTableParamModel param)
+        {
+            var placeList = _placeService.getAllPlace();
+            IEnumerable<Place> filteredListItems;
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                filteredListItems = placeList.Where(
+                    d => (d.Name != null && d.Name.ToLower().Contains(param.sSearch.ToLower()))
+                );
+            }
+            else
+            {
+                filteredListItems = placeList;
+            }
+            // Sort.
+            var sortColumnIndex = Convert.ToInt32(Request["iSortCol_0"]);
+            var sortDirection = Request["sSortDir_0"]; // asc or desc
+
+            switch (sortColumnIndex)
+            {
+                case 2:
+                    filteredListItems = sortDirection == "asc"
+                        ? filteredListItems.OrderBy(c => c.Name)
+                        : filteredListItems.OrderByDescending(c => c.Name);
+                    break;
+            }
+
+            var displayedList = filteredListItems.Skip(param.iDisplayStart).Take(param.iDisplayLength);
+            var result = displayedList.Select(c => new IConvertible[]{
+                c.Id,
+                c.PlaceImages.First().Image,
+                c.Name,
+                c.Description,
+                c.Address,
+                //c.Fields.First().FieldType.,
+            }.ToArray());
+
+            return Json(new
+            {
+                param.sEcho,
+                iTotalRecords = result.Count(),
+                iTotalDisplayRecords = filteredListItems.Count(),
+                aaData = result
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Entities/Services/PlaceService.cs
@@ -0,0 +1,23 @@
+﻿using SportsSocialNetwork.Models.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsSocialNetwork.Models.Entities.Services
+{
+    public partial interface IPlaceService
+    {
+        IEnumerable<Place> getAllPlace();
+    }
+    public partial class PlaceService : IPlaceService
+    {
+        public IEnumerable<Place> getAllPlace()
+        {
+            return this.GetActive();
+        }
+
+    }
+
+
+}
