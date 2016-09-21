7d29ab91e7dce56ed71c43a7414962948bcd323a
dev-huytv <huy.tran@wisky.vn>
2016-09-21 14:32:22 +0700

    [huytv] - user search place

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Entities/Services/FieldService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsSocialNetwork.Models.Entities.Services
+{
+    public partial class FieldService
+    {
+    }
+}
