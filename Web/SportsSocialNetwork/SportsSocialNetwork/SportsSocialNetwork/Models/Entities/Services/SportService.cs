e19cdfa0e95798868c24f6443a0731aa4d1ae234
dev-huytv <huy.tran@wisky.vn>
2016-09-23 14:18:58 +0700

    [huytv] - search place

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Entities/Services/SportService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsSocialNetwork.Models.Entities.Services
+{
+    public partial interface ISportService
+    {
+        IEnumerable<Sport> getAllSport();
+    }
+    public partial class SportService
+    {
+        public IEnumerable<Sport> getAllSport()
+        {
+            return this.GetActive();
+        }
+    }
+}
