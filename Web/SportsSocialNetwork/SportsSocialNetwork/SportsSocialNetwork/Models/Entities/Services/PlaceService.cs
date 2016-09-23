e0e39e5e731ca4c7585d2e8e2e57986be5e48e9a
dev-huytv <huy.tran@wisky.vn>
2016-09-23 16:58:13 +0700

    [huytv] - resolve conflig

--- Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Entities/Services/PlaceService.cs
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Entities/Services/PlaceService.cs
@@ -9,7 +9,7 @@ namespace SportsSocialNetwork.Models.Entities.Services
     {
 
         #region Code from here
-
+        IEnumerable<Place> getAllPlace();
 
 
         #endregion
@@ -20,7 +20,10 @@ namespace SportsSocialNetwork.Models.Entities.Services
     public partial class PlaceService: IPlaceService
     {
         #region Code from here
-
+        public IEnumerable<Place> getAllPlace()
+        {
+            return this.GetActive();
+        }
 
 
         #endregion
