bba486bdecfa079af22a3a6b94c22b6d2b341178
Nadreasky <nadreasky@gmail.com>
2016-09-26 11:16:29 +0700

    add old controller

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Areas/PlaceOwner/Controllers/PlaceController.cs
@@ -0,0 +1,48 @@
+﻿using SkyWeb.DatVM.Mvc.Autofac;
+using SportsSocialNetwork.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsSocialNetwork.Areas.PlaceOwner.Controllers
+{
+    public class PlaceController : Controller
+    {
+        // GET: PlaceOwner/Place
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PlaceDetail(int? id)
+        {
+            var _placeService = this.Service<IPlaceService>();
+            Place place = _placeService.FirstOrDefault(p => p.Id == id.Value);
+
+            return View(place);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult savePlace(Place place)
+        {
+            var _placeService = this.Service<IPlaceService>();
+            _placeService.savePlace(place);
+            return RedirectToAction("Index");
+        }
+
+        public string deletePlace(int id)
+        {
+            var _placeService = this.Service<IPlaceService>();
+            Place place = _placeService.FirstOrDefaultActive(p => p.Id == id);
+            if (place != null)
+            {
+                _placeService.Deactivate(place);
+                return "success";
+            }
+            return "false";
+        }
+    }
+}
