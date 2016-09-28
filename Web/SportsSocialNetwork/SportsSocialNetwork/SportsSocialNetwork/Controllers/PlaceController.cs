00d66a630653f87b3478d7a94d3ce48475e41f52
dev-huytv <huy.tran@wisky.vn>
2016-09-28 17:04:41 +0700

    [huytv] - search place by sport, city, district

--- Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Controllers/PlaceController.cs
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Controllers/PlaceController.cs
@@ -1,6 +1,9 @@
-﻿using SportsSocialNetwork.Models;
+﻿using SkyWeb.DatVM.Mvc;
+using SportsSocialNetwork.Models;
 using SportsSocialNetwork.Models.Entities;
 using SportsSocialNetwork.Models.Entities.Services;
+using SportsSocialNetwork.Models.ViewModels;
+using SportsSocialNetwork.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,31 +12,61 @@ using System.Web.Mvc;
 
 namespace SportsSocialNetwork.Controllers
 {
-    public class PlaceController : Controller
+    public class PlaceController : BaseController
     {
         // GET: Place
-        private readonly IPlaceService _placeService;
-        private readonly ISportService _sportService;
-
-        public PlaceController(IPlaceService placeService, ISportService sportService)
-        {
-            _placeService = placeService;
-            _sportService = sportService;
-        }
         public ActionResult Index()
         {
+            var _sportService = this.Service<ISportService>();
+            Country vietnam = AddressUtil.GetINSTANCE().GetCountry(Server.MapPath(AddressUtil.PATH));
             var sportList = _sportService.getAllSport();
-            ViewBag.sportList = sportList;
-            return View();
+            IEnumerable<SelectListItem> selectList = sportList.Select(s => new SelectListItem
+            {
+                Text = s.Name,
+                Value = s.Id.ToString()
+            }).ToArray();
+            ViewBag.SportList = selectList;
+
+            var province = vietnam.VietNamese.ToList();
+            IEnumerable<SelectListItem> provinceList = province.Select(m => new SelectListItem
+            {
+                Text = m.Type + " " + m.Name,
+                Value = m.Name
+            }).OrderBy(s => s.Value).ToArray();
+            ViewBag.ProvinceList = provinceList;
+            var viewModel =new  SearchPlaceViewModel();
+            return View(viewModel);
         }
 
-        public ActionResult GetData(JQueryDataTableParamModel param, int sportId)
+        public ActionResult GetDistrict(string provinceName)
         {
-            /*if (sportId != 0)
+            Country vietnam = AddressUtil.GetINSTANCE().GetCountry(Server.MapPath(AddressUtil.PATH));
+            var province = vietnam.VietNamese.Where(p => p.Name.Equals(provinceName)).ToList();
+            var district = province.First().Districts.ToList();
+            IEnumerable<SelectListItem> districtList = district.Select(m => new SelectListItem
             {
+                Text = m.Type +" "+ m.Name,
+                Value = m.Name
+            }).OrderBy(s => s.Value).ToArray();
 
-            }*/
+            return Json(districtList, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult GetData(JQueryDataTableParamModel param, string sport, string province, string district)
+        {
+            var _placeService = this.Service<IPlaceService>();
             var placeList = _placeService.getAllPlace();
+            var sportId = -1;
+            if (sport != null && sport != "")
+            {
+                sportId = Int32.Parse(sport);
+            } 
+            if(sportId != -1 || (province !=null && province != ""))
+            {
+                placeList = _placeService.getPlace(sportId, province, district);
+            }
+            
             IEnumerable<Place> filteredListItems;
             if (!string.IsNullOrEmpty(param.sSearch))
             {
---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/ViewModels/SearchPlaceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsSocialNetwork.Models.ViewModels
+{
+    public class SearchPlaceViewModel
+    {
+        public string SportId { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        
+    }
+}
