34932035022471b81cc8b1e25c2f1ebb7251cc38
ManhNV <ManhNV@MANHNVSE61271>
2016-10-14 11:34:18 +0700

    [ManhNV] - Update config email

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Utilities/Setting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace SportsSocialNetwork.Models.Utilities
+{
+    public class Setting
+    {
+        public static string CREDENTIAL_EMAIL = GetDataFromWebConfig("CredentialEmail");
+        public static string CREDENTIAL_PASSWORDS = GetDataFromWebConfig("CredentialPasswords");
+
+        private static string GetDataFromWebConfig(string key)
+        {
+            return WebConfigurationManager.AppSettings[key];
+        }
+    }
+}
