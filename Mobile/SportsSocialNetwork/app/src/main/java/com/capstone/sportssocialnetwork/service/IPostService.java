317e928f4a3cdaa91d20595fce9eec4db7ef1872
ManhNV <ManhNV@MANHNVSE61271>
2016-10-22 19:20:28 +0700

    [ManhNV] - Add post service

---
+++ Mobile/SportsSocialNetwork/app/src/main/java/com/capstone/sportssocialnetwork/service/IPostService.java
@@ -0,0 +1,18 @@
+package com.capstone.sportssocialnetwork.service;
+
+import com.capstone.sportssocialnetwork.model.Post;
+import com.capstone.sportssocialnetwork.model.response.ResponseModel;
+
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+
+/**
+ * Created by ManhNV on 10/22/16.
+ */
+public interface IPostService {
+//
+//    @Multipart
+//    @POST("/api/post/post")
+//    Call<ResponseModel<Post>> createPost()
+}
