745d1a2ebebec08943c04715b2e3e89f29d91854
Nadreasky <nadreasky@gmail.com>
2016-10-24 13:15:38 +0700

    [quyht] - update hashtag sport

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/ViewModels/PostSportDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SportsSocialNetwork.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsSocialNetwork.Models.ViewModels
+{
+    public class PostSportDetailViewModel : PostSportViewModel
+    {
+        public SportViewModel Sport { get; set; }
+    }
+}
