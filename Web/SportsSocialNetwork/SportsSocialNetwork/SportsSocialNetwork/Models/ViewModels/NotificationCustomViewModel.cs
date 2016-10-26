b4f5fad4379e2c7a3a68a8fc1e97c64b9e296ea9
MyPC <MyPC@MINHHXSE61475>
2016-10-26 16:09:22 +0700

    [minhhx] - add CreateDateString for LoadNoti api

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/ViewModels/NotificationCustomViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsSocialNetwork.Models.ViewModels
+{
+    public class NotificationCustomViewModel
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public int Type { get; set; }
+        public Nullable<int> PostId { get; set; }
+        public Nullable<int> InvitationId { get; set; }
+        public Nullable<int> OrderId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public String CreateDateString { get; set; }
+        public Nullable<bool> MarkRead { get; set; }
+        public bool Active { get; set; }
+    }
+}
