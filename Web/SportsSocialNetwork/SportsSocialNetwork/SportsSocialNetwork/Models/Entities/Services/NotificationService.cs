a86df2615c41e6e796cce468ea66d309f5c0bbe5
MyPC <MyPC@MINHHXSE61475>
2016-10-27 22:17:23 +0700

    [minhhx] - add notification to like api

--- Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Entities/Services/NotificationService.cs
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Entities/Services/NotificationService.cs
@@ -8,7 +8,7 @@ namespace SportsSocialNetwork.Models.Entities.Services
     public partial interface INotificationService
     {
         #region Code from here
-        Notification SaveNoti(String userId, String fromUserId, String title, String message, int type, Nullable<int> postId, Nullable<int> invitationId);
+        Notification SaveNoti(String userId, String fromUserId, String title, String message, int type, Nullable<int> postId, Nullable<int> invitationId, Nullable<int> orderId);
 
         IEnumerable<Notification> GetNoti(String userId, int skip, int take);
 
@@ -25,7 +25,7 @@ namespace SportsSocialNetwork.Models.Entities.Services
 
 
         #region Code from here
-        public Notification SaveNoti(string userId, String fromUserId, string title, string message, int type, Nullable<int> postId, Nullable<int> invitationId)
+        public Notification SaveNoti(string userId, String fromUserId, string title, string message, int type, Nullable<int> postId, Nullable<int> invitationId, Nullable<int> orderId)
         {
             Notification noti = new Notification();
             noti.UserId = userId;
@@ -36,6 +36,8 @@ namespace SportsSocialNetwork.Models.Entities.Services
             noti.PostId = postId;
             noti.CreateDate = DateTime.Now;
             noti.InvitationId = invitationId;
+            noti.OrderId = orderId;
+            noti.MarkRead = false;
             noti.Active = true;
             this.Create(noti);
             this.Save();
