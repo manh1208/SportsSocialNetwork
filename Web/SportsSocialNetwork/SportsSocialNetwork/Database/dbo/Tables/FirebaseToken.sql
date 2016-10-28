02167039ee7cfa05aaf01b0590468a8bc2ed4de3
ManhNV <ManhNV@MANHNVSE61271>
2016-10-28 15:30:56 +0700

    [ManhNv] - Update DB firebasetoken

--- Web/SportsSocialNetwork/SportsSocialNetwork/Database/dbo/Tables/FirebaseToken.sql
+++ Web/SportsSocialNetwork/SportsSocialNetwork/Database/dbo/Tables/FirebaseToken.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[FirebaseToken] (
-    [Id]     INT            NOT NULL,
+    [Id]     INT            IDENTITY (1, 1) NOT NULL,
     [UserId] NVARCHAR (128) NOT NULL,
     [Token]  NVARCHAR (MAX) NOT NULL,
     CONSTRAINT [PK_FirebaseToken] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_FirebaseToken_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
 );
+
+
 
