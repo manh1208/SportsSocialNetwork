feb442d6824c3427aacaaddab0ced5b470782f3a
MyPC <MyPC@MINHHXSE61475>
2016-10-28 16:13:56 +0700

    [minhhx] - fix save token

--- Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Notifications/NotificationModel.cs
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/Notifications/NotificationModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportsSocialNetwork.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +8,7 @@ namespace SportsSocialNetwork.Models.Notifications
 {
     public class NotificationModel
     {
-
+       public NotificationCustomViewModel noti { get; set; }
     }
 
     public class IOSNotification
