a9f78e44dbe417004dffc19a9e2fc1591a5cf36b
dev-huytv <huy.tran@wisky.vn>
2016-10-29 10:13:45 +0700

    [huy-tv] commit to merge with Quy

--- Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Controllers/LikeController.cs
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Controllers/LikeController.cs
@@ -2,6 +2,7 @@
 using SportsSocialNetwork.Models;
 using SportsSocialNetwork.Models.Entities;
 using SportsSocialNetwork.Models.Entities.Services;
+using SportsSocialNetwork.Models.Enumerable;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,20 +23,41 @@ namespace SportsSocialNetwork.Controllers
 
         public ActionResult LikeUnlikePost(int postId, String userId)
         {
-            var service = this.Service<ILikeService>();
+            var likeService = this.Service<ILikeService>();
+
+            var notiService = this.Service<INotificationService>();
+
+            var userService = this.Service<IAspNetUserService>();
 
             ResponseModel<Like> response = null;
             try
             {
-                bool result = service.LikeUnlikePost(postId, userId);
+                AspNetUser user = userService.FirstOrDefaultActive(x => x.Id == userId);
+
+                bool createNoti = false;
+
+                Like like = likeService.FirstOrDefault(x => x.PostId == postId && x.UserId == userId);
+
+                if (like == null && GetPostUserId(postId) != user.Id)
+                {
+                    createNoti = true;
+                }
+
+                like = likeService.LikeUnlikePost(postId, userId);
+
+                bool result = like.Active;
 
                 if (result)
                 {
-                    response = new ResponseModel<Like>(true, "Liked", null);
+                    response = new ResponseModel<Like>(true, "Đã thích", null);
+                    if (createNoti)
+                    {
+                        notiService.SaveNoti(GetPostUserId(postId), userId, "Like", user.FullName + " đã thích bài viết của bạn", (int)NotificationType.Post, postId, null, null);
+                    }
                 }
                 else
                 {
-                    response = new ResponseModel<Like>(true, "Unliked", null);
+                    response = new ResponseModel<Like>(true, "Đã bỏ thích", null);
                 }
             }
             catch (Exception)
@@ -43,6 +65,13 @@ namespace SportsSocialNetwork.Controllers
                 response = ResponseModel<Like>.CreateErrorResponse("Thao tác thất bại!", systemError);
             }
             return Json(response);
+        }
+
+        private String GetPostUserId(int postId)
+        {
+            var service = this.Service<IPostService>();
+
+            return service.FirstOrDefaultActive(x => x.Id == postId).UserId;
         }
     }
 }
