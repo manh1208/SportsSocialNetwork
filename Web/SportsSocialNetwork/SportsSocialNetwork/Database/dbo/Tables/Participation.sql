824e95847847f1afeaf2c545421d52d4f9ea18ec
ManhNV <ManhNV@MANHNVSE61271>
2016-10-31 20:32:16 +0700

    [ManhNv] - Update DB

--- Web/SportsSocialNetwork/SportsSocialNetwork/Database/dbo/Tables/Participation.sql
+++ Web/SportsSocialNetwork/SportsSocialNetwork/Database/dbo/Tables/Participation.sql
@@ -1,13 +1,17 @@
 ﻿CREATE TABLE [dbo].[Participation] (
-    [Id]       INT            IDENTITY (1, 1) NOT NULL,
-    [EventId]  INT            NOT NULL,
-    [UserId]   NVARCHAR (128) NOT NULL,
-    [Type]     INT            NOT NULL,
-    [TeamName] NVARCHAR (255) NULL,
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [EventId]     INT            NOT NULL,
+    [UserId]      NVARCHAR (128) NOT NULL,
+    [Type]        INT            NOT NULL,
+    [TeamName]    NVARCHAR (255) NULL,
+    [Slogan]      NVARCHAR (MAX) NULL,
+    [Description] NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Participation] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Participation_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]),
     CONSTRAINT [FK_Participation_Event] FOREIGN KEY ([EventId]) REFERENCES [dbo].[Event] ([Id])
 );
+
+
 
 
 
--- Web/SportsSocialNetwork/SportsSocialNetwork/Database/dbo/Tables/Place.sql
+++ Web/SportsSocialNetwork/SportsSocialNetwork/Database/dbo/Tables/Place.sql
@@ -1,25 +1,28 @@
 ﻿CREATE TABLE [dbo].[Place] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [UserId]      NVARCHAR (128) NOT NULL,
-    [Name]        NVARCHAR (50)  NOT NULL,
-    [Email]       VARCHAR (255)  NOT NULL,
-    [City]        NVARCHAR (255) NOT NULL,
-    [District]    NVARCHAR (255) NOT NULL,
-    [Ward]        NVARCHAR (255) NOT NULL,
-    [Address]     NVARCHAR (255) NOT NULL,
-    [PhoneNumber] VARCHAR (20)   NOT NULL,
-    [Description] NVARCHAR (MAX) NOT NULL,
-    [Latitude]    FLOAT (53)     NULL,
-    [Longitude]   FLOAT (53)     NULL,
-    [Avatar]      NVARCHAR (255) NOT NULL,
-    [Status]      INT            NOT NULL,
-    [Active]      BIT            NOT NULL,
-    [StartTime]   TIME (7)       NOT NULL,
-    [EndTime]     TIME (7)       NOT NULL,
-    [Approve]     BIT            NOT NULL,
+    [Id]                  INT            IDENTITY (1, 1) NOT NULL,
+    [UserId]              NVARCHAR (128) NOT NULL,
+    [Name]                NVARCHAR (50)  NOT NULL,
+    [Email]               VARCHAR (255)  NOT NULL,
+    [City]                NVARCHAR (255) NOT NULL,
+    [District]            NVARCHAR (255) NOT NULL,
+    [Ward]                NVARCHAR (255) NOT NULL,
+    [Address]             NVARCHAR (255) NOT NULL,
+    [PhoneNumber]         VARCHAR (20)   NOT NULL,
+    [Description]         NVARCHAR (MAX) NOT NULL,
+    [Latitude]            FLOAT (53)     NULL,
+    [Longitude]           FLOAT (53)     NULL,
+    [Avatar]              NVARCHAR (255) NOT NULL,
+    [AcceptPaymentOnline] BIT            NULL,
+    [Status]              INT            NOT NULL,
+    [Active]              BIT            NOT NULL,
+    [StartTime]           TIME (7)       NOT NULL,
+    [EndTime]             TIME (7)       NOT NULL,
+    [Approve]             BIT            NOT NULL,
     CONSTRAINT [PK_Place] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Place_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
 );
+
+
 
 
 
