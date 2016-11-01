115dfc730b1f5677abac9fef5a711c567c46fe42
ManhNV <ManhNV@MANHNVSE61271>
2016-11-01 16:53:02 +0700

    [ManhNv]- add challengesql

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/Database/dbo/Tables/Challenge.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Challenge] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [FromGroup]   INT            NOT NULL,
+    [ToGroup]     INT            NOT NULL,
+    [Accepted]    BIT            NULL,
+    [Status]      INT            NOT NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    [Active]      BIT            NOT NULL,
+    CONSTRAINT [PK_Challenge] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Challenge_Group] FOREIGN KEY ([FromGroup]) REFERENCES [dbo].[Group] ([Id]),
+    CONSTRAINT [FK_Challenge_Group1] FOREIGN KEY ([ToGroup]) REFERENCES [dbo].[Group] ([Id])
+);
+
