1a04f1294a2d46d62d8dc103f590d9bd55f08565
MyPC <MyPC@MINHHXSE61475>
2016-11-05 22:17:52 +0700

    [minhhx] - commit rebase

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Areas/API/Controllers/InvitationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsSocialNetwork.Areas.Api.Controllers
+{
+    public class InvitationController : Controller
+    {
+        // GET: Api/Invitation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
