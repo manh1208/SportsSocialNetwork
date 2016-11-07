916f73863de5011b50018b214a946c233752e2c1
dev-huytv <huy.tran@wisky.vn>
2016-11-07 11:36:06 +0700

    [huy-tv] git ignore

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Controllers/InvitationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsSocialNetwork.Controllers
+{
+    public class InvitationController : Controller
+    {
+        // GET: Invitation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
