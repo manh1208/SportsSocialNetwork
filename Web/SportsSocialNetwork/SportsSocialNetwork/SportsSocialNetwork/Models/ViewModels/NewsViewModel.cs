8599917d32b67a97cee4300ea2b3bf0a90c009e5
ManhNV <ManhNV@MANHNVSE61271>
2016-11-08 12:35:02 +0700

    [ManhNV] - Add news

---
+++ Web/SportsSocialNetwork/SportsSocialNetwork/SportsSocialNetwork/Models/ViewModels/NewsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsSocialNetwork.Models.ViewModels
+{
+    public partial class NewsViewModel
+    {
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+    }
+}
